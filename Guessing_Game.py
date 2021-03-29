8b2dfbc634d942f310a30e0faba772f9f4a8f348
HarshaSri-Sameera <70195328+HarshaSri-Sameera@users.noreply.github.com>
2021-03-29 12:07:37 +0530

    Create Guessing_Game.py
    Play the game once!

---
+++ Guessing_Game.py
@@ -0,0 +1,29 @@
+import random
+
+answer = random.randint(1, 11)
+print("""RULES TO PLAY GUESSING GAME:
+        1) Guess a number between 1 to 10
+        2) To exit the game press the number zero
+        """)
+print('okay! now guess any no. from 1 to 10:')
+guess = int(input())
+if guess == answer:                                    
+    print("you got it first time")
+    print("Excellent! you are good at guessing random numbers")
+else:
+    while guess != answer:
+        if guess < answer:
+            print("guess higher")
+        else:
+            print("guess lower")
+        guess = int(input())
+        if guess == 0 or guess > 10:
+            print('GAME OVER')
+            print('Answer:{}'.format(answer))
+            break
+        else:
+            if guess == answer:
+                print("well done! you guessed it correctly.")
+                break
+            else:
+                print("TRY AGAIN!(keep guessing)")
